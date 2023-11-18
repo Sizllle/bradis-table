e368e8644124b581592dec0f0f2d3cbfc29108b1
Sizllle <vladislaw.kuzminov2017@gmail.com>
2023-11-18 22:27:23 +0200

    Initial commit

---
+++ app/src/main/java/com/work/bradis_table/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.work.bradis_table
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
